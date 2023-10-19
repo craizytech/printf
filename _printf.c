df7ec79ff7d1d2bdc600fa7469c78b9d6c1fbc3f
craizytech <craizytech@gmail.com>
2023-10-19 18:05:09 +0300

    inserted a guard clause

--- _printf.c
+++ _printf.c
@@ -12,11 +12,11 @@ int _printf(const char *format, ...)
 	char *str;
 	int n;
 	int count = 0;
-
-	if (format == NULL)
-		return (0);
-
 	va_list args;
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
 	va_start(args, format);
 
 	while (*format)
