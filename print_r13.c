f06552e44f858a97c3a48cf2e185bad4370f389e
craizytech <craizytech@gmail.com>
2023-10-20 23:51:37 +0300

    Printf

---
+++ print_r13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_r13 - encrypts an input
+ * @args: variadic argument
+ * Return: length
+ */
+int print_r13(va_list args)
+{
+	int i, j, len = 0, k = 0;
+	char *str = va_arg(args, char *);
+	char alphabet[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char rot13key[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		k = 0;
+		for (j = 0; alphabet[j] && !k; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				_putchar(rot13key[j]);
+				len++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(str[i]);
+			len++;
+		}
+	}
+	return (len);
+}
